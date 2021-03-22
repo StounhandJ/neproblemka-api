af1300e93a574c997bdf5386bb90e8c311cd3d76
BlackJeket <bjeket@gmail.com>
2021-03-22 07:25:41 +0300

    added promocode create, update, del

--- controllers/promocodeController.js
+++ controllers/promocodeController.js
@@ -22,11 +22,23 @@ async function getAll(req, res){
     let promos = await promoCode.get_promoCodes(req.query.getAll)
     await renderingJson(res, JSON.stringify(promos) === JSON.stringify([])?200:404, promos)
 }
+async function create(req, res){
+    let promos = await promoCode.create_promoCode(req.query.name, req.query.codeName, req.query.discount, req.query.typeOfCode, req.query.limitUsing)
+    await renderingJson(res, JSON.stringify(promos) === JSON.stringify([])?400:200,promos)
+}
+
+async function update(req, res){
+    await renderingJson(res, 200, await promoCode.update_promoCode(req.query.id, req.query.name, req.query.codeName, req.query.discount, req.query.typeOfCode, req.query.limitUsing))
+}
+
+async function del(req, res){
+    await renderingJson(res, 200, await promoCode.delete_promoCode(req.query.id))
+}
 
 module.exports = {
     get: get,
-    getAll: getAll
-    // create: create,
-    // update: update,
-    // del: del
+    getAll: getAll,
+    create: create,
+    update: update,
+    del: del
 }
