2dcb9df862ec1217b06e9a2b08d859cca3be4d20
StounhandJ <rmarinicev@gmail.com>
2021-03-23 12:12:52 +0300

    update documentation

---
+++ models/chequeModel.js
@@ -0,0 +1,91 @@
+const Sequelize = require("sequelize")
+
+class pchequeModel{
+    static model
+
+    static connect(sequelize){
+        this.model = sequelize.define("cheque",{
+            id : {
+                type: Sequelize.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+                allowNull: false
+            },
+            idPaymentOrder  : {
+                type:Sequelize.INTEGER,
+                allowNull: false
+            },
+            amount : {
+                type:Sequelize.INTEGER,
+                allowNull: false,
+                unique: true
+            },
+            date : {
+                type:Sequelize.INTEGER,
+                allowNull: false
+            },
+            secretKey : {
+                type:Sequelize.CHAR(255),
+                allowNull: false
+            },
+            state:{
+                type:Sequelize.TINYINT,
+                allowNull: true
+            }
+        },{
+            timestamps: false,
+            tableName: "cheque"
+        })
+    }
+
+    static async create_cheque(idPaymentOrder, amount, secretKey)
+    {
+        return await this.model.create({
+            idPaymentOrder: idPaymentOrder,
+            amount: amount,
+            secretKey: secretKey,
+            date: Date.now()/1000
+        }).catch(() => {
+            return null
+        })
+    }
+
+    static async get_cheque_id(id)
+    {
+        return await this.model.findOne({
+            where:{
+                id: id
+            }
+        })
+    }
+
+
+    static async get_cheque_secretKey(secretKey)
+    {
+        return await this.model.findOne({
+            where:{
+                secretKey: secretKey
+            }
+        })
+    }
+
+    static async get_cheques()
+    {
+        const result = Object.values(await this.model.findAll({where: {state:0}}))
+        return result.length<1?null:result;
+    }
+
+    static async delete_cheque(id)
+    {
+        await this.model.update(
+            { state: 1 },
+            { where: { id: id } }
+        )
+    }
+}
+
+
+module.exports = (sequelize)=>{
+    promoCodeModel.connect(sequelize)
+    return promoCodeModel
+}
